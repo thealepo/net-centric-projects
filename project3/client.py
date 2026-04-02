0b61ce2a4c06a26fdb84a421389749c1b14fe134
thealepo <sanchezc.alexe@gmail.com>
2026-04-02 16:03:05 -0400

    skeleton code

---
+++ project3/client.py
@@ -0,0 +1,31 @@
+import socket
+
+def start_client():
+    print("Starting client...")
+
+    
+    print("Creating RSA keypair")
+    print("RSA keypair created")
+
+    print("Creating client socket")
+
+    print("Connecting to server")
+
+    print("Creating data socket")
+
+    print("Requesting tunnel")
+
+    print("Server public key received")
+
+    print("Tunnel established")
+
+    print("Encrypting message: Hello")
+
+    print(f"Sending encrypted message: {encrypted_message}")
+
+    print("Received hash")
+    print("Computing hash")
+    print("Secure")
+
+if __name__ == "__main__":
+    start_client()
---
+++ project3/server.py
@@ -0,0 +1,27 @@
+import socket
+
+def start_server():
+    print("Starting server...")
+
+    print("Creating RSA keypair")
+
+    print("RSA keypair created")
+
+    print("Creating server socket")
+
+    print("Awaiting connections...")
+
+    print("Connection requested. Creating data socket")
+
+    print("Tunnel requested. Sending public key")
+
+    print("Post requested.")
+
+    print(f"Decrypted message: {encrypted_message}")
+
+    print("Computing hash")
+
+    print(f"Responding with hash: {message_hash}")
+
+if __name__ == "__main__":
+    start_server()
